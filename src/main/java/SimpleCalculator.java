c9608e71d4c37a70e8c2c0b5432f457095ba2b02
JuliansRecent <juliansrecent@gmail.com>
2023-01-23 19:06:17 -0800

    Module complete. Prompt the user for two numbers. Calculate the result of the +, -, *, and / operators for the two numbers, then print each operation and result.

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -7,6 +7,20 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        // Prompt the user for two numbers
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        // Calculate the values for +, -, /, *
+        int additionResult = firstNumber + secondNumber;
+        int subtractionResult = firstNumber - secondNumber;
+        int multiplicationResult = firstNumber * secondNumber;
+        double divisionResult = 1.0 * firstNumber / secondNumber;
+        // Print operations and outputs for +, -, /, *
+        System.out.println(firstNumber + " + " + secondNumber + " = " + additionResult);
+        System.out.println(firstNumber + " - " + secondNumber + " = " + subtractionResult);
+        System.out.println(firstNumber + " * " + secondNumber + " = " + multiplicationResult);
+        System.out.println(firstNumber + " / " + secondNumber + " = " + divisionResult);
     }
 }
